6141c687d1f4ebb8960d3e757b7374b69eea0ed8
Matt Parlette <matthew.parlette@gmail.com>
2016-11-06 09:07:09 -0500

    Add Users from Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
 
   get 'cards/admin', to: 'cards#admin'
   get 'cards/:year/:month/:day', to: 'cards#date'
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
 
   resources :cards
 
-  root 'cards#index'
+  root to: 'cards#index'
 end
